34330973d51ed2ebd7f043dc70756eaf284d442f
Arth <arthursamuel1696@gmail.com>
2023-10-26 23:54:07 +0800

    add back button

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,15 +1,21 @@
 import { useSelector } from "react-redux";
 import { AuthState } from "../../store/User/store";
 import { Container, Wrapper } from "./Home.styles";
-import { LargeText } from "../../components/typography/Typography";
+import {
+  LargeText,
+  MediumText,
+  SmallText,
+} from "../../components/typography/Typography";
 import { Gutter } from "../../components/gutter/Gutter";
 import TodoCard from "./__components/todoCard/TodoCard";
 import ModalToDoCard from "./__components/modal/Modal";
 import { useState } from "react";
 import FloatButton from "./__components/floatButton/FloatButton";
 import React from "react";
+import { useNavigate } from "react-router";
 
 export default function Home() {
+  const navigate = useNavigate();
   const user = useSelector((state: AuthState) => state.user.user);
   const dataTodoUserCallback = React.useCallback(() => {
     const data = user?.records.find((item) => item.key === user.activeUser);
@@ -17,9 +23,18 @@ export default function Home() {
   }, [user]);
   const [visible, setVisible] = useState(false);
 
+  const handleGoBack = () => {
+    navigate("/login");
+  };
+
   return (
     <Container>
       <Wrapper>
+        <Gutter vertical={8}>
+          <SmallText onClick={handleGoBack} style={{ cursor: "pointer" }}>
+            To Login Page
+          </SmallText>
+        </Gutter>
         <LargeText>Hi, {user?.activeUser}</LargeText>
         <Gutter vertical={15}>
           {dataTodoUserCallback()?.map((item, index) => {
